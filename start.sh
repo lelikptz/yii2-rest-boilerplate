e4021691d79a821f799d30cf248ac93b1a525e06
Alexey Orlov <alexey@orlov.tech>
2020-03-29 23:45:58 +0300

    fix

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ docker-compose up -d
 chmod 755 ./init
 if [[ "$1" == "prod" ]]
 then
-docker exec -w /var/www/html rest-container sh -c "composer install --no-dev && ./init --env=Production --overwrite=All && ./yii migrate"
+docker exec -w /var/www/html rest-container sh -c "composer install --no-dev && ./init --env=Production --overwrite=All"
 else
-docker exec -w /var/www/html rest-container sh -c "composer install && ./init --env=Development --overwrite=All && ./yii migrate"
+docker exec -w /var/www/html rest-container sh -c "composer install && ./init --env=Development --overwrite=All"
 fi
